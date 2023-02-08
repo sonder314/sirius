2ac6546faf8535c9aeec174a663d43865562dcc6
ArinaSmuglienko <>
2023-02-08 12:28:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+def poland(s):
+  stack = []
+  product = 1
+  for i in s:
+    if i.isdigit():
+      stack.append(int(i))
+    else:
+      if len(stack) == 1:
+      product = calc(stack[-2], stack[-1], i)
+      del stack[-1]
+      del stack[-1]
+      break
+  for i in s:
+    if i.isdigit():
+      stack.append(int(i))
+    else:
+      product = calc(product, stack[-1], i)
+      del stack[-1]
+  return product
+
+
+def calc(a, b, s):
+  if s == '*':
+    return a * b
+  elif s == '+':
+    return a + b
+  return a - b
+
+s = '8 9 + 1 7 - *'
+s = ''.join(list(s.split(' ')))
+print(poland(s))
+
+#(a+b+c)*d = a b c + d*
